2d7b308e46365d0b3ae4b2d4593c749bbcb2c1c5
Labib2oo2 <67455237+Labib2oo2@users.noreply.github.com>
2021-07-02 10:13:15 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template ,request
+from flask import render_template
+import json
+app = Flask(__name__)
+
+	
+	
+	
+	
+
+
+@app.route("/")
+def main():
+	 	print("hi")
+	 	if type(request.args.get("d")) is str:
+	 		d= json.loads(request.args.get("d"))
+	 		f=open("player_scores.txt","a")
+	 		f.write(f"{d['name']} {d['score']}\n")
+	 		print(d['name'])
+	 	
+	 	return render_template("game.html")
+	 
+	 
+
+@app.route("/index")
+def index():
+	name="labib hasan"
+	return render_template("labib.html", name2= name)
+app.run(debug=True)
+
