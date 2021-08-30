d436df8cc631b957595714d72e04470c7899c2cb
Labib2oo2 <67455237+Labib2oo2@users.noreply.github.com>
2021-08-30 10:23:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -27,13 +27,13 @@ def index():
 
 import pymongo 
 import urllib
-client = pymongo.MongoClient("mongodb://Labib:"+urllib.parse.quote_plus("949802loveeve:>")+"@test-shard-00-00.y1zlb.mongodb.net:27017,test-shard-00-01.y1zlb.mongodb.net:27017,test-shard-00-02.y1zlb.mongodb.net:27017/test?ssl=true&replicaSet=atlas-a4yw33-shard-0&authSource=admin&retryWrites=true&w=majority")
+@app.route("/mon")
+def mon():
+	client = pymongo.MongoClient("mongodb://Labib:"+urllib.parse.quote_plus("949802loveeve:>")+"@test-shard-00-00.y1zlb.mongodb.net:27017,test-shard-00-01.y1zlb.mongodb.net:27017,test-shard-00-02.y1zlb.mongodb.net:27017/test?ssl=true&replicaSet=atlas-a4yw33-shard-0&authSource=admin&retryWrites=true&w=majority")
+	db=client.honululu
+	posts=db.labib
+	posts.insert_one({"name":"labib"})
 
-db=client.honululu
-
-posts=db.labib
-
-posts.insert_one({"name":"labib"})
 
 if __name__ == "__main__": 
 
